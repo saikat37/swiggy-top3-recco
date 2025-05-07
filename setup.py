4bd8f88a58df0aa0f4cbd176c6d969d9bd2e3416
Saikat Santra <saikatsantra6396@gmail.com>
2025-05-07 09:41:08 +0530

    Cookiecutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will help to reccomend top 3 food',
+    author='Saikat Santra',
+    license='',
+)
